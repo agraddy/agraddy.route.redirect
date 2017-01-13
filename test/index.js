feb77ab20e26ee69fc16502412c222aa3b8e1f10
Anthony Graddy <anthony@locationq.com>
2017-01-13 06:11:30 +0000

    Working

--- test/index.js
+++ test/index.js
@@ -1,8 +1,70 @@
 process.chdir('test');
 var tap = require('agraddy.test.tap')(__filename);
+var response = require('agraddy.test.res');
 
 var mod = require('../');
+var routes = {};
+routes.get = {};
 
-tap.assert.equal(mod.main('result'), 'result', 'Should be equal.');
+one();
+
+function one() {
+	var res = response();
+
+	mod(routes.get, 301, '/one', '/destination');
+
+	res.on('finish', function() {
+		tap.assert.equal(res._statusCode, 301, 'Should set redirect status code.');
+		two();
+	});
+
+	routes.get['/one']({}, res);
+}
+
+function two() {
+	var res = response();
+
+	mod(routes.get, 302, '/two', '/destination');
+
+	res.on('finish', function() {
+		tap.assert.equal(res._statusCode, 302, 'Should set redirect status code.');
+		regex();
+	});
+
+	routes.get['/two']({}, res);
+}
+
+function regex() {
+	var res = response();
+
+	mod(routes.get, 302, '^/matches(1*)/regex$', '/destination');
+
+	res.on('finish', function() {
+		tap.assert.equal(res._statusCode, 302, 'Should set redirect status code.');
+		plugins();
+	});
+
+	routes.get['^/matches(1*)/regex$']({}, res);
+}
+
+function plugins() {
+	var res = response();
+
+	mod(routes.get, 302, '/plugins', '/destination', [function plugin(req, res, lug, cb) {
+		tap.assert(true, 'Should run the luggage.');
+		cb();
+	}]);
+
+	res.on('finish', function() {
+		tap.assert.equal(res._statusCode, 302, 'Should set redirect status code.');
+		end();
+	});
+
+	routes.get['/plugins']({}, res);
+}
+
+function end() {
+}
+
 
 
